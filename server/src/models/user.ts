1cf819e3ab7a59f1059193a98dd04fec6378dbad
Benny Chrispin <ndizibaidu23@gmail.com>
2024-01-03 23:54:25 +0200

    complete Hashing Password

--- server/src/models/user.ts
+++ server/src/models/user.ts
@@ -1,5 +1,7 @@
+import { compare } from "bcrypt";
 // interface (typescript)
 
+import { compare, hash } from "bcrypt";
 import { Model, ObjectId, Schema, model } from "mongoose";
 
 interface UserDocument {
@@ -17,7 +19,11 @@ interface UserDocument {
   followings: ObjectId[];
 }
 
-const userSchema = new Schema<UserDocument>(
+interface Methods {
+  comparePassword(password: string): Promise<boolean>;
+}
+
+const userSchema = new Schema<UserDocument, {}, Methods>(
   {
     name: {
       type: String,
@@ -66,4 +72,18 @@ const userSchema = new Schema<UserDocument>(
   { timestamps: true }
 );
 
-export default model("User", userSchema) as Model<UserDocument>;
+userSchema.pre("save", async function (next) {
+  // hash the token
+  if (this.isModified("password")) {
+    this.password = await hash(this.password, 10);
+  }
+
+  next();
+});
+
+userSchema.methods.comparePassword = async function (password) {
+  const result = await compare(password, this.password);
+  return result;
+};
+
+export default model("User", userSchema) as Model<UserDocument, {}, Methods>;
