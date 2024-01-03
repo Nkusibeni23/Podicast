40588fa30430abaf6862628c9622ce9d2035e1f2
Benny Chrispin <ndizibaidu23@gmail.com>
2024-01-03 16:09:03 +0200

    Tool for email and sending first Email

--- server/src/controllers/user.ts
+++ server/src/controllers/user.ts
@@ -1,10 +1,44 @@
 import { CreateUser } from "#/@types/user";
 import { RequestHandler } from "express";
 import User from "#/models/user";
+import nodemailer from "nodemailer";
+import { MAILTRAP_PASS, MAILTRAP_USER } from "#/utils/variables";
 
 export const create: RequestHandler = async (req: CreateUser, res) => {
   const { email, password, name } = req.body;
 
   const user = await User.create({ name, email, password });
+
+  //   send verification email
+  //   const transport = nodemailer.createTransport({
+  //     host: "sandbox.smtp.mailtrap.io",
+  //     port: 2525,
+  //     auth: {
+  //       user: MAILTRAP_USER,
+  //       pass: MAILTRAP_PASS,
+  //     },
+  //   });
+
+  const transport = nodemailer.createTransport({
+    host: "sandbox.smtp.mailtrap.io",
+    port: 2525,
+    auth: {
+      user: "3d10d743c7722a",
+      pass: "a35ad182a8a5e2",
+      method: "PLAIN",
+    },
+  });
+
+  try {
+    transport.sendMail({
+      to: user.email,
+      from: "auth@gmail.com",
+      html: "<h1>Hello World!!</h1>",
+    });
+    console.log("Email sent successfully!");
+  } catch (error) {
+    console.error("Error sending email:", error);
+  }
+
   res.status(201).json({ user });
 };
--- server/src/utils/variables.ts
+++ server/src/utils/variables.ts
@@ -1,3 +1,3 @@
 const { env } = process as { env: { [key: string]: string } };
 
-export const MONGO_URL = env.MONGO_URL as string;
+export const { MONGO_URL, MAILTRAP_PASS, MAILTRAP_USER } = env;
