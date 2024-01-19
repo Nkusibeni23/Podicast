ee9d84f43f04bde01eb87179903312298a0c104c
Your GitHub Username <Your GitHub Email>
2024-01-20 00:28:10 +0200

    Auth Middleware

--- server/src/middleware/auth.ts
+++ server/src/middleware/auth.ts
@@ -1,5 +1,9 @@
 import passwordResetToken from "#/models/passwordResetToken";
 import { RequestHandler } from "express";
+import { JwtPayload, verify } from "jsonwebtoken";
+import User from "#/models/user";
+
+const jwtSecret = "baiduuuu";
 
 export const isValidPassResetToken: RequestHandler = async (req, res, next) => {
   const { token, userId } = req.body;
@@ -17,6 +21,30 @@ export const isValidPassResetToken: RequestHandler = async (req, res, next) => {
     return res
       .status(403)
       .json({ error: "Unauthorized access, Invalid token" });
+
+  next();
+};
+
+export const mustAuth: RequestHandler = async (req, res, next) => {
+  const { authorization } = req.headers;
+  const token = authorization?.split("Bearer")[1];
+  if (!token) return res.status(403).json({ error: "Unauthorized request!" });
+
+  const payload = verify(token, jwtSecret) as JwtPayload;
+  const id = payload.UserId;
+
+  const user = await User.findOne({ _id: id, tokens: token });
+  if (!user) return res.status(403).json({ error: "Unauthorized request!" });
+
+  req.user = {
+    id: user._id,
+    name: user.name,
+    email: user.email,
+    verified: user.verified,
+    avatar: user.avatar?.url,
+    followers: user.followers.length,
+    followings: user.followings.length,
+  };
 
   next();
 };
--- server/src/routers/auth.ts
+++ server/src/routers/auth.ts
@@ -7,7 +7,7 @@ import {
   updatePassword,
   verifyEmail,
 } from "#/controllers/user";
-import { isValidPassResetToken } from "#/middleware/auth";
+import { isValidPassResetToken, mustAuth } from "#/middleware/auth";
 import { validate } from "#/middleware/validate";
 import User from "#/models/user";
 
@@ -22,7 +22,7 @@ import { JWT_SECRET } from "#/utils/variables";
 import { Router } from "express";
 import { JwtPayload, verify } from "jsonwebtoken";
 
-const jwtSecret = "baiduuuu";
+// const jwtSecret = "baiduuuu";
 
 const router = Router();
 
@@ -46,27 +46,9 @@ router.post(
 
 router.post("/sign-in", validate(SignInValidationSchema), signIn);
 
-router.get("/is-auth", async (req, res) => {
-  const { authorization } = req.headers;
-  const token = authorization?.split("Bearer")[1];
-  if (!token) return res.status(403).json({ error: "Unauthorized request!" });
-
-  const payload = verify(token, jwtSecret) as JwtPayload;
-  const id = payload.UserId;
-
-  const user = await User.findById(id);
-  if (!user) return res.status(403).json({ error: "Unauthorized request!" });
-
+router.get("/is-auth", mustAuth, (req, res) => {
   res.json({
-    profile: {
-      id: user._id,
-      name: user.name,
-      email: user.email,
-      verified: user.verified,
-      avatar: user.avatar?.url,
-      followers: user.followers.length,
-      followings: user.followings.length,
-    },
+    profile: req.user,
   });
 });
 
