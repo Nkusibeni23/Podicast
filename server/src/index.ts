96112120da8e1f9133d02f51512477e776f283c6
Your GitHub Username <Your GitHub Email>
2024-01-29 11:57:30 +0200

    complete Uploading Audio

--- server/src/index.ts
+++ server/src/index.ts
@@ -3,15 +3,17 @@ import express from "express";
 import "dotenv/config";
 import "./db";
 import authRouter from "./routers/auth";
+import audioRouter from "./routers/audio";
 
 const app = express();
 
 // register our middleware
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
+app.use(express.static("src/public"));
 
 app.use("/auth", authRouter);
-app.use(express.static("src/public"));
+app.use("/audio", audioRouter);
 
 const PORT = process.env.PORT || 8989;
 app.listen(PORT, () => {
