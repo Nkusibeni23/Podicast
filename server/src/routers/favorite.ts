05e610309d228178c86a7906673b5588fd6e59a9
Your GitHub Username <Your GitHub Email>
2024-01-31 16:41:48 +0200

    Get is Fav

--- server/src/routers/favorite.ts
+++ server/src/routers/favorite.ts
@@ -1,4 +1,8 @@
-import { getFavorite, toggleFavorite } from "#/controllers/favorite";
+import {
+  getFavorite,
+  getIsFavorite,
+  toggleFavorite,
+} from "#/controllers/favorite";
 import { isVerified, mustAuth } from "#/middleware/auth";
 import { Router } from "express";
 
@@ -6,5 +10,6 @@ const router = Router();
 
 router.post("/", mustAuth, isVerified, toggleFavorite);
 router.get("/", mustAuth, getFavorite);
+router.get("/is-fav", mustAuth, getIsFavorite);
 
 export default router;
