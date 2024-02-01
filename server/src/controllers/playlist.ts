b1d14ba5f81c43871b33799f6d9b4ab30eb3d2f6
Your GitHub Username <Your GitHub Email>
2024-02-01 13:31:43 +0200

    getting playlist by profile

--- server/src/controllers/playlist.ts
+++ server/src/controllers/playlist.ts
@@ -4,6 +4,7 @@ import Playlist from "#/models/playlist";
 import { error } from "console";
 import { RequestHandler } from "express";
 import { isValidObjectId } from "mongoose";
+import { title } from "process";
 
 export const createPlaylist: RequestHandler = async (
   req: CreatePlayListRequest,
@@ -114,4 +115,22 @@ export const removePlaylist: RequestHandler = async (req, res) => {
   }
 
   res.json({ success: true });
+};
+
+export const getPlaylistByProfile: RequestHandler = async (req, res) => {
+  const data = await Playlist.find({
+    owner: req.user.id,
+    visibility: { $ne: "auto" },
+  }).sort("-createAt");
+
+  const playlist = data.map((item) => {
+    return {
+      id: item._id,
+      title: item.title,
+      itemsCount: item.items.length,
+      visibility: item.visibility,
+    };
+  });
+
+  res.json({ playlist });
 };
